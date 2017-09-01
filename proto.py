c2125f2ca1d749a143698777e6a86fb2a83c871d
Kim Changdong <best564@naver.com>
2017-09-01 12:19:49 +0900

    Add AheuiStorage class

--- proto.py
+++ proto.py
@@ -137,47 +137,8 @@ def install_global():
     global 앒, 앓, 암, 압, 앖, 앗, 았
     global 앙, 앚, 앛, 앜, 앝, 앞, 앟
 
-    global activated
-
-    global machine_storage
-
-    global output_buffer
-
-    global activate
-    global send_to
-    global pop_is_nonzero
-
-    global pop
-    global pop_print_char
-    global push_strict
-    global duplicate
-    global swap
-
-    global compare
-    global add
-    global sub
-    global mul
-    global div
-    global mod
-
-    global halt
+    global AheuiStorage
     global AheuiHalt
-
-    global send_to_catch
-    global pop_catch
-    global pop_print_char_catch
-    global duplicate_catch
-    global swap_catch
-    global compare_catch
-    global swap_catch
-    global add_catch
-    global sub_catch
-    global mul_catch
-    global div_catch
-    global mod_catch
-
-    global aheui_eval
-
 
     (
         아, 악, 앆, 앇, 안, 앉, 않,
@@ -185,8 +146,6 @@ def install_global():
         앒, 앓, 암, 압, 앖, 앗, 았,
         앙, 앚, 앛, 앜, 앝, 앞, 앟,
     ) = (chr(a + ord('아')) for a in range(28))
-
-    activated = 아
 
     def catch(err_class):
         def deco(f):
@@ -220,6 +179,8 @@ def install_global():
                 self.push(op(b, a))
             return f
         return deco
+
+    class AheuiHalt(Exception): pass
 
     class AheuiStorageMixin(object):
         @arithmetic(lambda b, a: int(b >= a))
@@ -300,126 +261,126 @@ def install_global():
         def swap(self):
             raise NotImplementedError
 
-    machine_storage = {
-        storage_id: (
-            AheuiQueue() if storage_id == 앙
-            else AheuiPipe() if storage_id == 앟
-            else AheuiStack()
-        )
-        for storage_id in (
-            아, 악, 앆, 앇, 안, 앉, 않,
-            앋, 알, 앍, 앎, 앏, 앐, 앑,
-            앒, 앓, 암, 압, 앖, 앗, 았,
-            앙, 앚, 앛, 앜, 앝, 앞, 앟,
-        )
-    }
+    class AheuiStorage(object):
+        def __init__(self):
+            self.activated = 아
+            self.output_buffer = []
+            self.machine_storage = {
+                storage_id: (
+                    AheuiQueue() if storage_id == 앙
+                    else AheuiPipe() if storage_id == 앟
+                    else AheuiStack()
+                )
+                for storage_id in (
+                    아, 악, 앆, 앇, 안, 앉, 않,
+                    앋, 알, 앍, 앎, 앏, 앐, 앑,
+                    앒, 앓, 암, 압, 앖, 앗, 았,
+                    앙, 앚, 앛, 앜, 앝, 앞, 앟,
+                )
+            }
 
-    output_buffer = []
+        def activate(self, storage_id):
+            self.activated = storage_id
+            return True
 
-    def activate(storage_id):
-        global activated
-        activated = storage_id
-        return True
+        def send_to(self, other_storage_id):
+            v = self.machine_storage[self.activated].pop()
+            self.machine_storage[other_storage_id].push(v)
+            return True
 
-    def send_to(other_storage_id):
-        v = machine_storage[activated].pop()
-        machine_storage[other_storage_id].push(v)
-        return True
+        def pop_is_nonzero(self):
+            v = self.machine_storage[self.activated].pop()
+            return v != 0
 
-    def pop_is_nonzero():
-        v = machine_storage[activated].pop()
-        return v != 0
+        def pop(self):
+            self.machine_storage[self.activated].pop()
+            return True
 
-    def pop():
-        machine_storage[activated].pop()
-        return True
+        def pop_print_char(self):
+            v = self.machine_storage[self.activated].pop()
+            self.output_buffer.append(v)
+            return True
 
-    def pop_print_char():
-        v = machine_storage[activated].pop()
-        output_buffer.append(v)
-        return True
+        def push_strict(self, val):
+            if val not in (0, 2, 3, 4, 5, 6, 7, 8, 9):
+                raise ValueError
+            return self.push(val)
 
-    def push_strict(val):
-        if val not in (0, 2, 3, 4, 5, 6, 7, 8, 9):
-            raise ValueError
-        return push(val)
+        def push(self, val):
+            self.machine_storage[self.activated].push(val)
+            return True
 
-    def push(val):
-        machine_storage[activated].push(val)
-        return True
+        def duplicate(self):
+            self.machine_storage[self.activated].duplicate()
+            return True
 
-    def duplicate():
-        machine_storage[activated].duplicate()
-        return True
+        def swap(self):
+            self.machine_storage[self.activated].swap()
+            return True
 
-    def swap():
-        machine_storage[activated].swap()
-        return True
+        def compare(self):
+            self.machine_storage[self.activated].compare()
+            return True
 
-    def compare():
-        machine_storage[activated].compare()
-        return True
+        def add(self):
+            self.machine_storage[self.activated].add()
+            return True
 
-    def add():
-        machine_storage[activated].add()
-        return True
+        def sub(self):
+            self.machine_storage[self.activated].sub()
+            return True
 
-    def sub():
-        machine_storage[activated].sub()
-        return True
+        def mul(self):
+            self.machine_storage[self.activated].mul()
+            return True
 
-    def mul():
-        machine_storage[activated].mul()
-        return True
+        def div(self):
+            self.machine_storage[self.activated].div()
+            return True
 
-    def div():
-        machine_storage[activated].div()
-        return True
+        def mod(self):
+            self.machine_storage[self.activated].mod()
+            return True
 
-    def mod():
-        machine_storage[activated].mod()
-        return True
+        def halt(self):
+            raise AheuiHalt
 
-    def halt():
-        raise AheuiHalt
+        catch_typeerror = catch(TypeError)
 
-    class AheuiHalt(Exception): pass
+        send_to_catch = catch_typeerror(send_to)
+        pop_catch = catch_typeerror(pop)
+        pop_print_char_catch = catch_typeerror(pop_print_char)
+        duplicate_catch = catch_typeerror(duplicate)
+        swap_catch = catch_typeerror(swap)
+        compare_catch = catch_typeerror(compare)
+        add_catch = catch_typeerror(add)
+        sub_catch = catch_typeerror(sub)
+        mul_catch = catch_typeerror(mul)
+        div_catch = catch_typeerror(div)
+        mod_catch = catch_typeerror(mod)
 
-    catch_typeerror = catch(TypeError)
+        del catch_typeerror
 
-    send_to_catch = catch_typeerror(send_to)
-    pop_catch = catch_typeerror(pop)
-    pop_print_char_catch = catch_typeerror(pop_print_char)
-    duplicate_catch = catch_typeerror(duplicate)
-    swap_catch = catch_typeerror(swap)
-    compare_catch = catch_typeerror(compare)
-    add_catch = catch_typeerror(add)
-    sub_catch = catch_typeerror(sub)
-    mul_catch = catch_typeerror(mul)
-    div_catch = catch_typeerror(div)
-    mod_catch = catch_typeerror(mod)
-
-    aheui_command_map = {
-        '다': add,
-        '따': mul,
-        '맣': pop_print_char,
-        '바': lambda: push_strict(0),
-        '박': lambda: push_strict(2),
-        '반': lambda: push_strict(2),
-        '받': lambda: push_strict(3),
-        '발': lambda: push_strict(5),
-        '밝': lambda: push_strict(7),
-        '밞': lambda: push_strict(9),
-        '밣': lambda: push_strict(8),
-        '밤': lambda: push_strict(4),
-        '밦': lambda: push_strict(6),
-        '빠': duplicate,
-        '타': sub,
-    }
-
-    def aheui_eval(commands):
-        for command in commands:
-            aheui_command_map[command]()
+        def aheui_eval(self, commands):
+            aheui_command_map = {
+                '다': self.add,
+                '따': self.mul,
+                '맣': self.pop_print_char,
+                '바': lambda: self.push_strict(0),
+                '박': lambda: self.push_strict(2),
+                '반': lambda: self.push_strict(2),
+                '받': lambda: self.push_strict(3),
+                '발': lambda: self.push_strict(5),
+                '밝': lambda: self.push_strict(7),
+                '밞': lambda: self.push_strict(9),
+                '밣': lambda: self.push_strict(8),
+                '밤': lambda: self.push_strict(4),
+                '밦': lambda: self.push_strict(6),
+                '빠': self.duplicate,
+                '타': self.sub,
+            }
+            for command in commands:
+                aheui_command_map[command]()
 
 
 def generate_data_area(s):
@@ -437,12 +398,13 @@ def generate_data_area(s):
 
 if __name__ == '__main__':
     install_global()
+    s = AheuiStorage()
     with open('code_area.aheui', 'rb') as fr:
         code_area = fr.read().decode('utf-8')
-    aheui_eval(generate_data_area(code_area))
+    s.aheui_eval(generate_data_area(code_area))
     try:
         # XXX
-        quine_code_area(__import__(__name__))
+        quine_code_area(s)
     except AheuiHalt:
         pass
     else:
@@ -451,5 +413,5 @@ if __name__ == '__main__':
     with open('quine.aheui', 'wb') as fw:
         fw.write(''.join(
             chr(cc)
-            for cc in output_buffer
+            for cc in s.output_buffer
         ).encode('utf-8'))
